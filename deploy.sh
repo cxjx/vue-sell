fed8497658769703eabdb0541889aa28be21112d
cxjx <648932803@qq.com>
2018-11-16 14:38:09 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# jenkins路径
-JENKINS_PATH="/home/common/jenkins"
-# Jenkins变量
+# 用户
 LINUX_USER="chenx"
-
-pwd
-# /home/common/jenkins/workspace/chenx_vue-sell
-
 # 用户家目录
-LINUX_HOME=`su - $LINUX_USER -c 'echo $HOME'`
+LINUX_HOME="/home/$LINUX_USER"
 # vue-sell项目部署路径
 WEB_PATH="$LINUX_HOME/nginx/html/vue-sell"
 
@@ -22,8 +16,7 @@ if [ $RETVAL = 0 ]; then
 
   # copy
   cp -r dist $WEB_PATH
-  chown -R ${LINUX_USER}:turing $WEB_PATH
 
-  # nginx reload
+  # reload
   #$LINUX_HOME/nginx/sbin/nginx -s reload
 fi
